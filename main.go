9dd39ab972cd42a15a3a5bbf2b605cdfe595a91b
realityone <realityone@me.com>
2017-10-22 10:00:13 +0800

    add update param

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -27,7 +28,7 @@ func goRepoPath(gopath string, url *git.URL) string {
 	return filepath.Join(gopath, "src", url.Host, repoPath)
 }
 
-func cloneRepo(rawurl, gopath string) {
+func fetchRepo(rawurl, gopath string, requireUpdate bool) {
 	url, err := git.ParseURL(rawurl)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to parse url `%s`: %s\n", rawurl, err)
@@ -40,27 +41,57 @@ func cloneRepo(rawurl, gopath string) {
 		os.Exit(1)
 	}
 
-	cloneCmd := exec.Command("git", "clone", rawurl, path)
-	cloneCmd.Stdout = os.Stdout
-	cloneCmd.Stderr = os.Stderr
-	if err := cloneCmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to clone repository `%s`: %s\n", rawurl, err)
-		os.Exit(1)
+	clone := func() {
+		cmd := exec.Command("sh", "-c", fmt.Sprintf(`
+			git clone %s
+			`, path))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to clone repository `%s`: %s\n", rawurl, err)
+			os.Exit(1)
+		}
 	}
+
+	update := func() {
+		cmd := exec.Command("sh", "-c", fmt.Sprintf(`
+			cd %s && \
+			git pull && \
+			git reset --hard HEAD
+			`, path))
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to update repository `%s`: %s\n", rawurl, err)
+			os.Exit(1)
+		}
+	}
+
+	if _, err := os.Stat(filepath.Join(path, ".git")); !os.IsNotExist(err) && requireUpdate {
+		update()
+		return
+	}
+
+	clone()
 }
 
 func usage() {
-	fmt.Println("git goget [REPOSITORY...]")
+	fmt.Println("usage: git goget [-u] [REPOSITORY...]")
 	os.Exit(1)
 }
 
 func main() {
 	gopath := gopath()
-	if len(os.Args) <= 1 {
+
+	update := flag.Bool("u", false, "update the package")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1 {
 		usage()
 	}
 
-	for _, repoURL := range os.Args[1:] {
-		cloneRepo(repoURL, gopath[0])
+	for _, repoURL := range args {
+		fetchRepo(repoURL, gopath[0], *update)
 	}
 }
